2f18f27fc9defe88e2be6a5a2009e24746404c57
Lucas-Hr <126322847+Lucas-Hr@users.noreply.github.com>
2023-04-18 18:29:21 +0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from './pages/Home'
+import { Market } from './pages/market'
+import  Layout  from './pages/Layout'
+import { User } from './pages/user'
+import { basename } from 'node:path/win32';
+// import { DealerRegister } from './pages/dealerRegister'
+// import { FarmerRegister } from './pages/farmerRegister'
+// import { Login } from './pages/login'
+
+
+function App() {  
+  return (
+    <div className='corps'>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="Market" element={<Market />} />
+            <Route path="User" element={<User />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
