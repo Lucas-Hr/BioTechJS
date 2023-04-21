6986b69e2e83c75b44a1bd6230a8af450e30c3ed
Lucas-Hr <126322847+Lucas-Hr@users.noreply.github.com>
2023-04-21 12:16:48 +0300

    Revert "Jo modif"

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import React from "react";
 import {Outlet} from "react-router-dom";
 import { Navbar } from "./../components/navbar";
@@ -13,34 +11,4 @@ const Layout = () => {
   );
 };
 
-=======
-import React from "react";
-import {Outlet} from "react-router-dom";
-import { Navbar } from "./../components/navbar";
-
-const Layout = () => {
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
-  );
-};
-
->>>>>>> d3124d65f8c66ceabfe96f3963571411dc4649a9
-=======
-import React from "react";
-import {Outlet} from "react-router-dom";
-import { Navbar } from "./../components/navbar";
-
-const Layout = () => {
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
-  );
-};
-
->>>>>>> 9c0eb3c84cc1723aeb4b7f574eb7440a412bf58f
 export default Layout;
